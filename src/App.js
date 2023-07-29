7aeb551b5c2e436de582095ccd2856ab4cb5a7cb
hartmandy <120344775+hartmandy@users.noreply.github.com>
2023-07-28 21:42:32 -0400

    added dummy data

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import NewExpense from "./components/NewExpense/NewExpense";
 const DUMMY_EXPENSES = [
   {
     id: "e1",
-    title: "Toilet Paper",
+    title: "Books",
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: "e3",
-    title: "Car Insurance",
+    title: "Keyboard",
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
   {
     id: "e4",
-    title: "New Desk (Wooden)",
+    title: "Cookie Dough",
     amount: 450,
     date: new Date(2021, 5, 12),
   },
